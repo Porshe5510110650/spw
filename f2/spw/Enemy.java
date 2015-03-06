0d20feb4edbb61e4d1572c7144945ff0a21778c5
Porshe5510110650 <porshe_b2@hotmail.com>
2015-03-06 19:31:14 +0700

    Update Enemy.java

--- f2/spw/Enemy.java
+++ f2/spw/Enemy.java
@@ -6,7 +6,7 @@ import java.awt.Graphics2D;
 
 public class Enemy extends Sprite{
 	public static final int Y_TO_FADE = 400;
-	public static final int Y_TO_DIE = 400;
+	public static final int Y_TO_DIE = 600;
 	
 	private int step = 12;
 	private boolean alive = true;
@@ -32,7 +32,7 @@ public class Enemy extends Sprite{
 	public void proceed(){
 		y += step;
 		if(y > Y_TO_DIE){
-			alive = false;
+			alive = true;
 		}
 	}
 	
