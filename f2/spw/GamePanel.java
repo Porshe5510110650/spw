9d2f221fe29885202616edac3e00ac17681192c8
Porshe5510110650 <porshe_b2@hotmail.com>
2015-04-21 20:39:12 +0700

    Update GamePanel.java

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -17,13 +17,13 @@ public class GamePanel extends JPanel {
 	public GamePanel() {
 		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
 		big = (Graphics2D) bi.getGraphics();
-		big.setBackground(Color.WHITE);
+		big.setBackground(Color.BLACK);
 	}
 
 	public void updateGameUI(GameReporter reporter){
 		big.clearRect(0, 0, 400, 600);
 		
-		big.setColor(Color.GREEN);		
+		big.setColor(Color.YELLOW);		
 		big.drawString(String.format("%08d", reporter.getScore()), 300, 20);
 		big.drawString(String.format("Combo : %03d", reporter.getCombo()), 260, 40);
 		for(Sprite s : sprites){
