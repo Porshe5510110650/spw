2139edd9ae01864d15e73f6e802f404c2dfcd4d6
Porshe5510110650 <porshe_b2@hotmail.com>
2015-04-28 20:14:38 +0700

    Update GamePanel.java

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -10,6 +10,10 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private BufferedImage bi;	
 	Graphics2D big;
 	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
@@ -23,21 +27,25 @@ public class GamePanel extends JPanel {
 	public void updateGameUI(GameReporter reporter){
 		big.clearRect(0, 0, 400, 600);
 		
-		big.setColor(Color.YELLOW);		
+		big.setColor(Color.WHITE);
 		big.drawString(String.format("Score %08d", reporter.getScore()), 260, 20);
 		big.drawString(String.format("Combo : %03d", reporter.getCombo()), 260, 40);
+		big.drawString(String.format("HP : %03d", reporter.getHp()), 150, 20);
+		big.drawString(String.format("Nuclear : %01d", reporter.getNc()), 20, 20);
+		big.drawString(String.format("Life Point : %01d", reporter.getLp()), 20, 40);
 		for(Sprite s : sprites){
 			s.draw(big);
 		}
-		
 		repaint();
+		
+		
 	}
 
 	@Override
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		int s0 = 0;
-		g2d.drawImage(bi, null, 0, 0);
+		g2d.drawImage(bi, null, s0, 0);
 	}
 
 }
