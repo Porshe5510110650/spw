b4409095d6662690fe0bb6335d0b15fa0150a941
Porshe5510110650 <porshe_b2@hotmail.com>
2015-04-28 20:23:07 +0700

    Update GameEngine.java

--- f2/spw/GameEngine.java
+++ f2/spw/GameEngine.java
@@ -59,7 +59,7 @@ public class GameEngine implements KeyListener, GameReporter{
 	}
 	
 	private void generateEnemy(){
-		Enemy e = new Enemy((int)(Math.random()*390), 30);
+		Enemy e = new Enemy((int)(Math.random()*400), 50);
 		gp.sprites.add(e);
 		enemies.add(e);
 	}
