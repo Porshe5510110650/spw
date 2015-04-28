d2561dfe076a9ad02c357d6f5dd53880e4abf813
Porshe5510110650 <porshe_b2@hotmail.com>
2015-04-28 20:16:17 +0700

    Update SpaceShip.java

--- f2/spw/SpaceShip.java
+++ f2/spw/SpaceShip.java
@@ -3,14 +3,17 @@ package f2.spw;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+
 public class SpaceShip extends Sprite{
 
-	int step = 30;
-	
+	int step = 8;
 	public SpaceShip(int x, int y, int width, int height) {
 		super(x, y, width, height);
 		
+		
 	}
+	
+	
 
 	@Override
 	public void draw(Graphics2D g) {
