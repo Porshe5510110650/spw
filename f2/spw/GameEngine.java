02807c64e4ce20852f613077dac35a6efd2f9843
Vongolax <porshe_b2@hotmail.com>
2015-05-18 09:10:58 +0700

    Revert "Update GameEngine.java"
    This reverts commit e429023a837f55debae52c94a43855b5ff91e135.

--- f2/spw/GameEngine.java
+++ f2/spw/GameEngine.java
@@ -81,7 +81,7 @@ public class GameEngine implements KeyListener, GameReporter{
 				gp.sprites.remove(e);
 				
 			}
-			if(!e.isHit()){
+			if(e.isHit()){
 				combo++;
 				if(combo>10){
 					score += 0.5;
